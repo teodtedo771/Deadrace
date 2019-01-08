e46ce2094442580bdfdc7e33c022e9bde137b95a
Georgi <georgiqwertyuiop@mail.bg>
2019-01-08 23:05:03 +0200

    контроли w,a,s,d

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,31 +5,38 @@ using System;
 
 public class playerMovement : MonoBehaviour
 {
-  
-    public float speed = 0;
+
+    public float speed = 2;
+    public float maxspeed = 10f;
     private float Angle = 0f;
+    private Vector3 moving;
     private Rigidbody CarRigidbody;
     void Start()
     {
         CarRigidbody = GetComponent<Rigidbody>();
-  
-    }
 
+    }
+    private void Update()
+    {
+       
+            
+        
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log(CarRigidbody.velocity.magnitude);
 
         float VerticalMove = Input.GetAxis("Vertical");
-      
         if (Math.Abs(Input.GetAxis("Horizontal")) > 0.1f)
         {
 
-            if (Math.Abs(Angle) < 80)
+            if (Math.Abs(Angle) < 40)
             {
                 Angle += Input.GetAxis("Horizontal");
             }
         }
-        else if (Math.Abs(Angle) < 0.5f)
+        else if (Math.Abs(Angle) < 1f)
         {
             Angle = 0;
         }
@@ -37,38 +44,39 @@ public class playerMovement : MonoBehaviour
         {
             Angle -= (Math.Abs(Angle) / Angle) / 2;
         }
-        Move(VerticalMove, Angle);
-    }
-
-    private void Move(float VerticalMove, float Angle)
-    {
-        Console.WriteLine(Input.GetAxis("Vertical"));
-
-        if (Math.Abs(Input.GetAxis("Vertical")) < 0.1f)
-        {
-            if (speed > 0.2f)
-            {
-                speed -= 0.2f;
-            }
-            else
-            {
-                speed = 0;
-            }
-        }
-
-        if (Math.Abs(Input.GetAxis("Vertical")) > 0.1f && speed < 10f) {
-            speed+=0.2f;
-
-        }
-
-
-        if (speed < 40) { speed += 0.2f; }
-         CarRigidbody.MovePosition(transform.position+Vector3.forward*Time.deltaTime*speed);
-        // CarRigidbody.MoveRotation(new Quaternion(0f, 0f, 0f, 0f));
-        transform.Rotate(0, Angle, 0);
-
+         Move(VerticalMove, Angle);
 
        
+        
+
+    }
+
+    void Move(float VerticalMove, float Angle)
+    {
+
+
+
+        if (VerticalMove > 0.1f )
+        {
+            CarRigidbody.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * 10 * speed);
+
+        }
+        if (VerticalMove < -0.1f)
+        {
+            CarRigidbody.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) *-10 * speed);
+        }
+
+
+        //Не пипай!!!
+        //за изпипване на физиките!!!!
+        //----------------------------------------------------------------------------------
+        // CarRigidbody.MovePosition(transform.position+Vector3.forward*Time.deltaTime*speed);
+        // CarRigidbody.MoveRotation(new Quaternion(0f, 0f, 0f, 0f));
+        //transform.Rotate(0, Angle / 100, 0);
+        //CarRigidbody.AddRelativeTorque(new Vector3(0,Angle,0));
+        //CarRigidbody.velocity = Quaternion.Euler(0, Angle/10f, 0) * CarRigidbody.velocity;
+        //(Input.GetAxis("Horizontal") * rotateSpeed
+        transform.Rotate(0,Angle/200,0);
     }
 
 }
